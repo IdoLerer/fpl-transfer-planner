fc09c7ef3aec8c50b6782718d22ef48624df02cc
IdoLerer <idolerer222@gmail.com>
2022-02-28 00:46:34 +0200

    Add list filetering

--- fpl-transfer-planner/src/PlayersList/players_list.tsx
+++ fpl-transfer-planner/src/PlayersList/players_list.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table'
 import { Position, Team } from '../Constants'
 import Player from '../Player';
 import Shirt from '../Shirt/shirt';
@@ -11,28 +12,46 @@ type PlayersListProps = {
 
 const renderPlayerItem = (player: Player) => {
     return (
-        <ListGroup.Item key={player.id}>
-            <div className="d-flex align-items-center">
-                <div className={styles.ShirtWrapper}>
+        <tr key={player.id}>
+            <td>
+                <div className={`${styles.ShirtWrapper} align-middle`}>
                     <Shirt team={player.team} position={player.position} width={'35'} />
                 </div>
-                <div className={styles.PlayerDetails}>
+                <div className={`${styles.PlayerDetails} align-middle`}>
                     <div className={styles.PlayerName}>{player.name}</div>
                     {Team[player.team]}
                 </div>
-            </div>
-        </ListGroup.Item >
+            </td>
+            <td className='align-middle'>{`£${(player.cost/10).toFixed(1)}m`}</td>
+            <td className='align-middle'>{player.id}</td>
+        </tr>
     )
 }
 
 const PlayersList = ({ availablePlayers }: PlayersListProps) => {
+    const [nameFilter, setNameFilter] = useState('');
 
     return (
-        <div className={styles.PlayersList}>
-            <ListGroup>
-                {availablePlayers.map(renderPlayerItem)}
-            </ListGroup>
-        </div>
+        <>
+            <input
+                type="text"
+                value={nameFilter}
+                onChange={e => setNameFilter(e.target.value.toLowerCase())} />
+            <div className={styles.PlayersList}>
+                <Table hover  size='sm'>
+                    <thead >
+                        <tr>
+                            <th  className='position-sticky  text-white'>Player</th>
+                            <th  className='position-sticky  text-white'>£</th>
+                            <th  className='position-sticky  text-white'>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody className='border-top-0'>
+                        {availablePlayers.filter(player => player.name.toLowerCase().includes(nameFilter)).sort((playerA, playerB) => playerB.cost - playerA.cost).map(renderPlayerItem)}
+                    </tbody>
+                </Table>
+            </div>
+        </>
     )
 }
 
